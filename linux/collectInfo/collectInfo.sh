944d07c5b987be1615c4ca83a2f5eb09737e515d
wili4m <wiliamjf@gmail.com>
2025-12-05 18:17:55 -0300

    changed var names

--- linux/collectInfo/collectInfo.sh
+++ linux/collectInfo/collectInfo.sh
@@ -6,14 +6,16 @@
 # Identifies OS version:
 debian_release="/etc/debian_version"
 centos_release="/etc/centos-release"
-ol_release="/etc/oracle-release"
 rhel_release="/etc/redhat-release"
+ol_release="/etc/oracle-release"
+lsb_release="/etc/lsb-release"
+os_release="/etc/os-release"
 
 generic_os_version() {
 
-    os_name="$(cat /etc/os-release | grep -w ^NAME | sed -e "s/NAME=\"//g" -e "s/\"//g")"
-    os_version="$(cat /etc/os-release | grep -w ^VERSION | sed -e "s/VERSION=\"//g" -e "s/\"//g")"
-    os_release="$os_name $os_version"
+    os_name="$(cat $os_info | grep -w ^NAME | sed -e "s/NAME=\"//g" -e "s/\"//g")"
+    os_version="$(cat $os_info | grep -w ^VERSION | sed -e "s/VERSION=\"//g" -e "s/\"//g")"
+    os_info="$os_name $os_version"
 
 }
 
@@ -21,20 +23,20 @@ generic_os_version() {
 if [ -e $debian_release ]; then
 
     # Checking if it's a Debian-Like:
-    if [ -e "/etc/lsb-release" ]; then
+    if [ -e "$lsb_release" ]; then
 
-        os_description="$(cat /etc/lsb-release | grep -w ^DISTRIB_DESCRIPTION | sed -e "s/DISTRIB_DESCRIPTION=\"//g" -e "s/\"//g")"
+        os_description="$(cat $lsb_release | grep -w ^DISTRIB_DESCRIPTION | sed -e "s/DISTRIB_DESCRIPTION=\"//g" -e "s/\"//g")"
 
         if [[ ! -z $os_description ]]; then
 
-            os_release="$os_description"
+            report_os_version="$os_description"
 
         fi
 
     else
  
         # Checking if it's Ubuntu for the second time:
-        os_name=$(cat /etc/os-release | grep -w ^NAME | sed -e "s/NAME=\"//g" -e "s/\"//g")
+        os_name=$(cat $os_release | grep -w ^NAME | sed -e "s/NAME=\"//g" -e "s/\"//g")
 
         if [[ $(echo $os_name | grep -i ubuntu) ]]; then
 
@@ -43,7 +45,8 @@ if [ -e $debian_release ]; then
    	 else
 
             # However, if it's not a Ubuntu, it's a Debian, so we'll collect just the release:
-            os_release=$(cat $debian_release)
+            report_os_version=$(cat $debian_release)
+
         fi
 
     fi
@@ -51,20 +54,20 @@ if [ -e $debian_release ]; then
 else
 
     # Verifies if it's a CentOS:
-    if [ -e $centos_release ]; then
+    if [ -e $centreport_os_version ]; then
 
         # Collect the release:
-        os_release=$(cat $centos_release)
+        report_os_version=$(cat $centreport_os_version)
 
     # Oracle Linux
     elif [ -e $ol_release ]; then
 
-        os_release=$(cat $ol_release)
+        report_os_version=$(cat $ol_release)
 
     # RHEL:
     elif [ -e $rhel_release ]; then
 
-        os_release=$(cat $rhel_release)
+        report_os_version=$(cat $rhel_release)
 
     else
 
@@ -76,9 +79,9 @@ else
 fi
 
 # Se o SO nao pode ser identificado, printa saida do comando uname -o
-if [[ ! -z $os_release ]]; then
+if [[ ! -z $report_os_version ]]; then
 
-    echo $os_release
+    echo $report_os_version
 
 else
 
